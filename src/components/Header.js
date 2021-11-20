06d378c440608cbdfe0e9b791bee6dc1ce89d8ea
Navsharan Cheema <ns.cheema@yahoo.com>
2021-11-20 23:15:40 +1100

    Header and Button components
    Added default props and prop types too.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+// rafc
+const Header = ({ title }) => {
+    const onClick = () => {
+        console.log ('Click')
+    }
     return (
-        <header>
-            <h1>Task Tracker</h1>
+        <header className='header'>
+            <h1>{ title }</h1>
+            <Button color='green' text='Add' onClick={ onClick }/>
         </header>
+        
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
