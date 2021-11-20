a503b7fb67d3171df131e1629005c11dc5c2a901
Navsharan Cheema <ns.cheema@yahoo.com>
2021-11-21 00:16:21 +1100

    Functionality to Delete Task
    Delete the task when clicked on x and added a default message for no task.

--- src/App.js
+++ src/App.js
@@ -23,10 +23,17 @@ const App = () => {
         reminder: false,
     }
 ])
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !==id))
+  }
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      {/* 'onDelete' is a Prop of 'Tasks' */} 
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask }/> : 'No Tasks To Show'}
     </div>
   );
 }
