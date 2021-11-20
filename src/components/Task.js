7cce01b5b45e7f04810e15cabb959b6e270aca7c
Navsharan Cheema <ns.cheema@yahoo.com>
2021-11-20 23:27:08 +1100

    Installed react icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,10 @@
-
+import { FaTimes } from 'react-icons/fa'
 const Task = ({ task }) => {
     return (
         <div className='task'>
-            <h3>{task.text}</h3>
+            <h3>
+                {task.text} <FaTimes style={{ color: 'red', cursor: 'pointer' }}/>
+            </h3>
             <p>{task.day}</p>
         </div>
     )
