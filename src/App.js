1fa166c0c5428769552589714b07b3a8afb7a92d
Navsharan Cheema <ns.cheema@yahoo.com>
2021-11-23 21:01:41 +1100

    Created AddTask component
    Add text in the form using 'useState' hook.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
+import AddTask from './components/AddTask';
 
 const App = () => {
   const [tasks, setTasks] = useState([
@@ -37,6 +38,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
+      <AddTask />
       {/* 'onDelete' is a Prop of 'Tasks' */} 
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask } onToggle={ toggleReminder }/> : 'No Tasks To Show'}
     </div>
