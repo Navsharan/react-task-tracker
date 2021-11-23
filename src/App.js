0adf8d6396f04fe30fea100bce65cbbcf67841d9
Navsharan Cheema <ns.cheema@yahoo.com>
2021-11-23 20:26:34 +1100

    Added 'Toggle Reminder'
    We can double click on an item to toggle.

--- src/App.js
+++ src/App.js
@@ -29,11 +29,16 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !==id))
   }
 
+  // Toggle Remminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
   return (
     <div className="container">
       <Header />
       {/* 'onDelete' is a Prop of 'Tasks' */} 
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask }/> : 'No Tasks To Show'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={ deleteTask } onToggle={ toggleReminder }/> : 'No Tasks To Show'}
     </div>
   );
 }
